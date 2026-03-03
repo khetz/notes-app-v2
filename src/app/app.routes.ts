62a45fa766ec38fb303afaa3edad9591dc5b8b4f
khetz <vukheta99@gmail.com>
2026-03-03 17:45:06 +0200

    Configured basic app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/register/register.component/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: RegisterComponent },
+    { path: 'register', component: RegisterComponent}
+];
