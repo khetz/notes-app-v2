bca5d11787001c5b339768fab84e7b91781ea8bc
khetz <vukheta99@gmail.com>
2026-03-03 14:54:59 +0200

    Added registration form definition and validation

--- src/app/features/auth/register/register.component/register.component.ts
+++ src/app/features/auth/register/register.component/register.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { equalPasswordFieldsValidator } from '../../../../shared/custom-validators';
 
 @Component({
   selector: 'app-register.component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+
+  private formBuilder = inject(FormBuilder);
+  registrationForm!: FormGroup;
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      username: [''],
+      password: [''],
+      confirmPassword: ['']
+    },
+    {validators: equalPasswordFieldsValidator})
+  }
+}
