60d7dc3fb36c1c91398985c2e625711a8873e2bf
khetz <vukheta99@gmail.com>
2026-03-10 19:30:13 +0200

    Added login component route definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register.component/register.component';
+import { LoginComponent } from './features/auth/login/login.component/login.component';
 
 export const routes: Routes = [
     { path: '', component: RegisterComponent },
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent }
 ];
