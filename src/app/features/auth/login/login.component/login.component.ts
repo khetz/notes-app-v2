bc4981c2add95a2d21c554870c59ca2f8413d02f
khetz <vukheta99@gmail.com>
2026-03-11 07:40:35 +0200

    Added navigation to register page from login and, implemented login form submission

--- src/app/features/auth/login/login.component/login.component.ts
+++ src/app/features/auth/login/login.component/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login.component',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
   private formBuilder = inject(FormBuilder);
   loginForm!: FormGroup;
+  private authService = inject(AuthService);
+  private destroyRef = inject(DestroyRef);
+  private routerService = inject(Router);
 
     ngOnInit() {
       this.loginForm = this.formBuilder.group({
@@ -19,6 +25,12 @@ export class LoginComponent implements OnInit {
   }
 
   submitLoginForm() {
-    
+    this.authService.login(this.loginForm.value)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.routerService.navigateByUrl('/home')
+        }
+      })
   }
 }
