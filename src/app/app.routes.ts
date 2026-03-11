c92e883b33fa45274749a9574d87ae07c3de97a3
khetz <vukheta99@gmail.com>
2026-03-11 07:39:55 +0200

    Chnaged default route to login component and configured home component path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register.component/register.component';
 import { LoginComponent } from './features/auth/login/login.component/login.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: RegisterComponent },
+    { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent }
 ];
