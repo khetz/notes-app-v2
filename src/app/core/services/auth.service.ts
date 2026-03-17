09743f6b722384456a665e823d9d8e3255856c67
khetz <vukheta99@gmail.com>
2026-03-17 21:47:24 +0200

    Added access token getter and setter in auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -8,9 +8,22 @@ import { environment } from '../../../environments/environment';
 export class AuthService {
   authUrl = environment.apiUrl + 'auth/'
   http = inject(HttpClient);
+  private accessToken: string | null = null;
 
   registerUser(filledRegistrationForm: Auth) {
     return this.http.post(`${this.authUrl}register`, filledRegistrationForm);
+  }
+
+  getAccessToken() {
+    return this.accessToken;
+  }
+
+  setAccessToken(token: string) {
+   this.accessToken = token; 
+  }
+
+  clearToken() {
+    this.accessToken = null;
   }
 
   login(filledLoginForm: Auth) {
