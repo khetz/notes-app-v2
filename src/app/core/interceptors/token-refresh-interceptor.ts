c32d3620cfc08352253bbf8902c11dc0ddaa1712
khetz <vukheta99@gmail.com>
2026-03-17 22:03:46 +0200

    Added token refresh on 401

--- src/app/core/interceptors/token-refresh-interceptor.ts
+++ src/app/core/interceptors/token-refresh-interceptor.ts
@@ -1,12 +1,20 @@
 import { HttpEventType, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
 import { tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const tokenRefreshInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  
   return next(req).pipe(
     tap((event) => {
       if (event.type == HttpEventType.Response 
         && event.status == HttpStatusCode.Unauthorized) {
-        // call token refresh function
+          authService.refreshToken().subscribe({
+            next: (res) => {
+              authService.setAccessToken(res.accessToken);
+            }
+          })
       }
     })
   );
