f9feea55eef66a29952f1187d8526f6cef6b9c38
khetz <vukheta99@gmail.com>
2026-03-17 23:36:59 +0200

    ensured refresh is only called once and handled redirection

--- src/app/core/interceptors/token-refresh-interceptor.ts
+++ src/app/core/interceptors/token-refresh-interceptor.ts
@@ -2,18 +2,41 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, tap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const tokenRefreshInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  
+  const routerService = inject(Router);
+  let isRefreshing = false;
+
+  if (req.url.includes('/refresh')) {
+    isRefreshing = true;
+    return next(req);
+  }
+
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      if (error.status == 401) {
-          authService.refreshToken().subscribe({
-            next: (res) => {
-              authService.setAccessToken(res.accessToken);
-            }
-          })
+      if (error.status == 401 && !isRefreshing) {
+        authService.refreshToken().subscribe({
+          next: (res) => {
+            authService.setAccessToken(res.accessToken);
+
+            const authorizedRequest = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${res.accessToken}`
+              }
+            });
+
+            return next(authorizedRequest);
+          },
+          error: (err) => {
+              authService.clearToken();
+              routerService.navigateByUrl('/login');
+          },
+          complete: () => {
+            isRefreshing = false;
+          }
+        })
       }
 
       return throwError(() => error);
