7b1f6d243337f4315e413d15a5a1684c4f4418b4
khetz <vukheta99@gmail.com>
2026-03-23 21:10:20 +0200

    Added notes page

---
+++ src/app/features/notes/pages/notes-page/notes-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-page',
+  imports: [],
+  templateUrl: './notes-page.html',
+  styleUrl: './notes-page.css',
+})
+export class NotesPage {}
