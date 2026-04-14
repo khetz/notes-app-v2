24107b177abc2b482e5507f5de3e0ca5dce438b8
khetz <vukheta99@gmail.com>
2026-04-14 10:50:58 +0200

    Updated sidebar referernce and removed app borders

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../../features/notes/components/sidebar.component/sidebar.component';
+import { SidebarComponent } from '../../../features/categories/components/sidebar.component/sidebar.component';
 import { NoteList } from '../../../features/notes/components/note-list/note-list';
 
 @Component({
