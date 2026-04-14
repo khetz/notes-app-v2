c5a581a18b1c0a3f497f9d9d65fdd26d6707824b
khetz <vukheta99@gmail.com>
2026-04-14 17:42:28 +0200

    Added category emission to home component

--- src/app/features/categories/components/sidebar.component/sidebar.component.ts
+++ src/app/features/categories/components/sidebar.component/sidebar.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { CategoriesService } from '../../services/categories.service';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { CategoryService } from '../../services/categories.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,11 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-  categoryService = inject(CategoriesService);
+  categoryService = inject(CategoryService);
   selectedCategoryId: number | null = null;
   categories$ = this.categoryService.getCategories();
+  @Output() categorySelected = new EventEmitter<number | null>();
 
   setSelectedCategoryId(categoryId: number | null) {
     this.selectedCategoryId = categoryId;
+    this.categorySelected.emit(this.selectedCategoryId);
   }
 }
