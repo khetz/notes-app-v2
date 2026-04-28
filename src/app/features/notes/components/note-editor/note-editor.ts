2e40754c11978dec8bcce77d30a5ba79d4317aae
khetz <vukheta99@gmail.com>
2026-04-28 17:31:15 +0200

    Added note editor component

---
+++ src/app/features/notes/components/note-editor/note-editor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-editor',
+  imports: [],
+  templateUrl: './note-editor.html',
+  styleUrl: './note-editor.css',
+})
+export class NoteEditor {}
