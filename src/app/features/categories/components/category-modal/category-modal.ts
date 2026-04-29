db845cc61d1135047b59d755b4428bacc77cdc02
khetz <vukheta99@gmail.com>
2026-04-29 18:25:31 +0200

    Added category modal component

---
+++ src/app/features/categories/components/category-modal/category-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-modal',
+  imports: [],
+  templateUrl: './category-modal.html',
+  styleUrl: './category-modal.css',
+})
+export class CategoryModal {}
